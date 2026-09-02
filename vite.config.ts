bb83d3c9be2dd37ffeef57c42d62035dc37a302e
ShafranovKirill <kirshafranov@gmail.com>
2026-09-02 13:28:12 +0300

    fix: project setting, add utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,13 +8,16 @@ import tailwindcss from '@tailwindcss/vite'
 import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import Icons from 'unplugin-icons/vite'
-import { customCollectionsList, customIconCollections } from './src/utils/icons'
 import IconsResolver from 'unplugin-icons/resolver'
-import path from 'node:path'
+
+import { customCollectionsList, customIconCollections } from './src/utils/icons.js'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig(({ mode }) => {
   const rootDir = path.resolve(__dirname, './')
   const loadedEnv = loadEnv(mode, rootDir)
+
   return {
     envDir: rootDir,
     envPrefix: 'VITE_',
