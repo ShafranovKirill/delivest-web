4e04386212ce5feaa4f2d711b91b78f65d1f4a83
ShafranovKirill <kirshafranov@gmail.com>
2026-09-02 13:18:34 +0300

    feat: deps and utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,45 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+import Components from 'unplugin-vue-components/vite'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
+import Icons from 'unplugin-icons/vite'
+import { customCollectionsList, customIconCollections } from './src/utils/icons'
+import IconsResolver from 'unplugin-icons/resolver'
+import path from 'node:path'
 
-export default defineConfig({
-  plugins: [vue(), ,],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const rootDir = path.resolve(__dirname, './')
+  const loadedEnv = loadEnv(mode, rootDir)
+  return {
+    envDir: rootDir,
+    envPrefix: 'VITE_',
+    server: {
+      port: Number(loadedEnv.VITE_PORT_WEB) || 5173,
     },
-  },
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+      Components({
+        resolvers: [
+          PrimeVueResolver(),
+          IconsResolver({
+            customCollections: customCollectionsList,
+          }),
+        ],
+      }),
+      Icons({
+        customCollections: customIconCollections,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
