a50aa7fb1a5943f2e02e2f47bfb19563ef2d48c4
kiruxashafr <kirshafranov@gmail.com>
2026-09-08 21:04:29 +0300

    fix: category bar

--- src/modules/menu/components/category/DesktopCategoryBar.vue
+++ src/modules/menu/components/category/DesktopCategoryBar.vue
@@ -66,7 +66,7 @@ watch(categories, updateVisibleCount, { deep: true })
     <div class="bg-white rounded-t-full w-full min-h-20">
       <div
         ref="containerRef"
-        class="whitespace-nowrap flex gap-4 max-w-7xl mx-auto py-3 px-12 2xl:px-2 items-center"
+        class="whitespace-nowrap flex gap-4 max-w-7xl mx-auto py-6 px-12 2xl:px-2 items-center"
       >
         <Button
           v-for="(category, index) in categories.slice(0, visibleCount)"
--- src/modules/menu/components/category/MobileCategoryBar.vue
+++ src/modules/menu/components/category/MobileCategoryBar.vue
@@ -18,13 +18,13 @@ useMobCatBarScroll(activeCategoryId, containerRef)
 </script>
 <template>
   <div class="bg-gray-200 w-full sticky top-0 z-50">
-    <div class="py-2 bg-white rounded-t-4xl w-full overflow-hidden">
+    <div class="py-4 bg-white rounded-t-4xl w-full overflow-hidden">
       <div ref="containerRef" class="flex overflow-x-auto scrollbar-none max-w-7xl mx-auto">
         <Button
           v-for="category in categories"
           :key="category.id"
           :data-id="category.id"
-          class="shrink-0 transition-all duration-200 border-none! ml-2"
+          class="shrink-0 transition-all duration-200 border-none! ml-4"
           @click="menuStore.scrollToCategory(category.id)"
           :severity="menuStore.isActive(category.id) ? 'primary' : 'secondary'"
           :outlined="!menuStore.isActive(category.id)"
