41d1357f27e415fa844b1eeb8f1efc38e1519cc9
kiruxashafr <kirshafranov@gmail.com>
2026-09-10 11:05:38 +0300

    fix: product modal

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import BranchSelectModal from '@/modules/branch/components/BranchSelectModal.vue'
+import { useViewportStore } from '@/modules/shared/stores/viewport.store'
 import ContactModal from '@/modules/widgets/contacts/components/ContactModal.vue'
 import MainFooter from '@/modules/widgets/footer/MainFooter.vue'
 import MainHeader from '@/modules/widgets/header/MainHeader.vue'
 import TopBar from '@/modules/widgets/header/TopBar.vue'
 import MainSidebar from '@/modules/widgets/sidebar/MainSidebar.vue'
+
+const viewportStore = useViewportStore()
 </script>
 <template>
   <div
     class="min-h-screen flex flex-col mx-auto items-center bg-white rounded-4xl lg:rounded-b-[70px]"
   >
-    <TopBar />
+    <TopBar v-if="!viewportStore.isMobile" />
     <MainHeader />
 
     <main class="w-full mx-auto">
--- src/modules/menu/components/product/ProductCard.vue
+++ src/modules/menu/components/product/ProductCard.vue
@@ -7,20 +7,24 @@ const props = defineProps<{ product: MenuProduct }>()
 const menuStore = useMenuStore()
 const { getImageUrl, handleImageError } = useImagePlaceholder()
 </script>
+
 <template>
   <Card
     @click="menuStore.openProductModal(props.product)"
-    class="group w-full h-full shrink-0 grow-0 rounded-4xl! p-2 overflow-hidden hover:cursor-pointer shadow-none!"
+    class="group w-full h-full flex flex-col shrink-0 grow-0 rounded-4xl! p-2 overflow-hidden hover:cursor-pointer shadow-none!"
   >
     <template #content>
-      <img
-        :src="getImageUrl(product.photo_url)"
-        @error="handleImageError"
-        class="w-full mb-2 object-cover aspect-square rounded-4xl block transition-transform duration-500 ease-in-out group-hover:scale-107"
-      />
-      <div class="flex flex-col items-center gap-2">
-        <p class="text-xl font-bold text-center">{{ product.name }}</p>
-        <Button class="rounded-4xl! bg-primary py-1.5! px-4! flex items-center gap-2">
+      <div class="flex flex-col h-full items-center justify-between">
+        <div class="flex flex-col items-center w-full">
+          <img
+            :src="getImageUrl(product.photo_url)"
+            @error="handleImageError"
+            class="w-full mb-2 object-cover aspect-square rounded-4xl block transition-transform duration-500 ease-in-out group-hover:translate-y-1.5"
+          />
+          <p class="text-xl font-bold text-center mb-2">{{ product.name }}</p>
+        </div>
+
+        <Button class="rounded-4xl! bg-primary py-1.5! px-4! flex items-center gap-2 mt-auto">
           <span class="text-lg font-bold text-white"> {{ product.price }} ₽ </span>
 
           <span
@@ -36,7 +40,11 @@ const { getImageUrl, handleImageError } = useImagePlaceholder()
 </template>
 
 <style scoped>
-:deep(.p-card-body) {
+:deep(.p-card-body),
+:deep(.p-card-content) {
   padding: 0 !important;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
